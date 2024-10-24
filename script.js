282702da77bbf6dfdbe65470bd9d229a09ab53e6
Divine Orji <dpkreativ@gmail.com>
2024-10-24 23:10:01 +0100

    the game works on a basic level, now let's make it fun

--- script.js
+++ script.js
@@ -4,28 +4,36 @@
  */
 
 const content = [
-    {question: "Gombe's capital",
-        answer: 'gombe',
-        score: 6
-    },
-    {
-        question: "The meal you eat between breakfast and lunchtime",
-        answer: 'brunch',
-        score: 2
-    },
-    {
-        question: "A frontend language adds interactivity to a web page?",
-        answer: 'javascript',
-        score: 3
-    },
-    {
-        question: "The fastest non-flight bird in the world",
-        answer: 'ostrich',
-        score: 4
-    },
-    {
-        question: "A global network of computers",
-        answer: 'internet',
-        score: 3
-    },
-]
+  { question: "Gombe's capital", answer: "gombe", score: 6 },
+  {
+    question: "The meal you eat between breakfast and lunchtime",
+    answer: "brunch",
+    score: 2,
+  },
+  {
+    question: "A frontend language adds interactivity to a web page?",
+    answer: "javascript",
+    score: 3,
+  },
+  {
+    question: "The fastest non-flight bird in the world",
+    answer: "ostrich",
+    score: 4,
+  },
+  {
+    question: "A global network of computers",
+    answer: "internet",
+    score: 3,
+  },
+];
+
+let totalScore = 0;
+
+for (let i = 0; i < content.length; i++) {
+  let userInput = prompt(content[i].question);
+
+  if (userInput.toLowerCase() === content[i].answer) {
+    alert(`Correct! Your score is ${totalScore + content[i].score}`);
+    totalScore = totalScore + content[i].score;
+  }
+}
