5c140f5eaa9008f7beeef9275d9c65978a9ea4a9
Divine Orji <dpkreativ@gmail.com>
2024-10-24 22:43:55 +0100

    add questions

--- script.js
+++ script.js
@@ -2,3 +2,30 @@
  * Create a one-word quiz game
  * - use only prompt, alert. no DOM manipulation
  */
+
+const content = [
+    {question: "Gombe's capital",
+        answer: 'gombe',
+        score: 6
+    },
+    {
+        question: "The meal you eat between breakfast and lunchtime",
+        answer: 'brunch',
+        score: 2
+    },
+    {
+        question: "A frontend language adds interactivity to a web page?",
+        answer: 'javascript',
+        score: 3
+    },
+    {
+        question: "The fastest non-flight bird in the world",
+        answer: 'ostrich',
+        score: 4
+    },
+    {
+        question: "A global network of computers",
+        answer: 'internet',
+        score: 3
+    },
+]
